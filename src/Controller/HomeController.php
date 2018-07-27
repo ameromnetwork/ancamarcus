ba287b1751a52a31c29e288186c70046e600d152
Johhny <johhny@epcvip.com>
2018-07-27 12:24:29 +0000

    Added the Routing annotation + another recipie

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response('<strong>Anca</strong>');
+    }
+}
