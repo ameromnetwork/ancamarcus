ecfd5682eb342170c2e57bb5265a72ad8ea87ffa
Johhny <johhny@epcvip.com>
2018-08-04 06:25:00 +0000

    Added TC

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,9 +12,21 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home_index")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index()
     {
         return $this->render('Homepage/index.html.twig');
+    }
+
+    /**
+     * @Route("/tc", name="tc")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function tc()
+    {
+        return $this->render('Homepage/tc.html.twig');
     }
 }
