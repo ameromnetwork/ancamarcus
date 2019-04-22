42e1bb40958a0393dbfb8031ee6012ea92f51903
Razvan <razvan.melnic@gmail.com>
2019-04-22 22:27:31 +0300

    Fix category_id column name

--- src/Migrations/Version20190422185905.php
+++ src/Migrations/Version20190422185905.php
@@ -22,7 +22,7 @@ final class Version20190422185905 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE blog_post ADD image VARCHAR(255) NOT NULL, ADD tags VARCHAR(255) NOT NULL, ADD category VARCHAR(255) NOT NULL, CHANGE author_id author_id INT DEFAULT NULL, CHANGE created_at created_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE blog_post ADD image VARCHAR(255) NOT NULL, ADD tags VARCHAR(255) NOT NULL, ADD category_id VARCHAR(255) NOT NULL, CHANGE author_id author_id INT DEFAULT NULL, CHANGE created_at created_at DATETIME NOT NULL');
         $this->addSql('ALTER TABLE user CHANGE salt salt VARCHAR(255) DEFAULT NULL, CHANGE facebook_id facebook_id VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE workout_program CHANGE parent_id parent_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL, CHANGE description description VARCHAR(255) DEFAULT NULL, CHANGE media_body media_body VARCHAR(100) DEFAULT NULL, CHANGE data data JSON DEFAULT NULL');
         $this->addSql('ALTER TABLE contact CHANGE subject subject VARCHAR(255) DEFAULT NULL');
@@ -35,7 +35,7 @@ final class Version20190422185905 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE author CHANGE phone phone VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE facebook facebook VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE twitter twitter VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE github github VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
-        $this->addSql('ALTER TABLE blog_post DROP image, DROP tags, DROP category, CHANGE author_id author_id INT DEFAULT NULL, CHANGE created_at created_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE blog_post DROP image, DROP tags, DROP category_id, CHANGE author_id author_id INT DEFAULT NULL, CHANGE created_at created_at DATETIME NOT NULL');
         $this->addSql('ALTER TABLE contact CHANGE subject subject VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
         $this->addSql('ALTER TABLE `user` CHANGE salt salt VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE facebook_id facebook_id VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci');
         $this->addSql('ALTER TABLE workout_program CHANGE parent_id parent_id INT DEFAULT NULL, CHANGE user_id user_id INT DEFAULT NULL, CHANGE description description VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE media_body media_body VARCHAR(100) DEFAULT \'NULL\' COLLATE utf8mb4_unicode_ci, CHANGE data data LONGTEXT DEFAULT NULL COLLATE utf8mb4_bin');
